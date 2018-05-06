0f253684552f5b8f006f339eeb50a22b2e121d97
Takashi <37811319+Team-TSMJC-GitHub@users.noreply.github.com>
2018-05-07 07:09:51 +1200

    Add files via upload

---
+++ EnemyFireMissile1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFireMissile1 : MonoBehaviour {
+
+    public GameObject enemyMissilePf;
+    public float missileSpeed;
+    private int timeCount = 0;
+
+    void Update()
+    {
+
+        timeCount += 1;
+        
+
+        if (timeCount % 60 == 0)
+        {
+
+            // generate enemy's missile
+            GameObject enemyMissile = Instantiate(enemyMissilePf, transform.position, Quaternion.identity) as GameObject;
+
+            Rigidbody enemyMissileRb = enemyMissile.GetComponent<Rigidbody>();
+
+            // decide the direction of missile."right" is point at the x axis.
+            enemyMissileRb.AddForce(transform.right * missileSpeed);
+
+            // eliminate the missile after 8 sec
+            Destroy(enemyMissile, 8.0f);
+
+        }
+    }
+}
