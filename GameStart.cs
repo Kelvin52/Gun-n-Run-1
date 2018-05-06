79a9ffd10db42b2fdcfcbf86baa1107e9438196d
Takashi <37811319+Team-TSMJC-GitHub@users.noreply.github.com>
2018-05-07 07:39:18 +1200

    Add files via upload

---
+++ GameStart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+	
+
+public class GameStart : MonoBehaviour
+{
+
+ 
+    //Game start
+    public void OnGameStartButtonClicked()
+    {
+        //load to main scenes
+        SceneManager.LoadScene("Main");
+    }
+}
+
